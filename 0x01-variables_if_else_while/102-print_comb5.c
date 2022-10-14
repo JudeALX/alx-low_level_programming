98691d7b39d8b226a36bd53ded570e54af7ddbe6
JudeALX <okonkwoemekajude>
2022-10-14 05:51:53 -0700

    updated 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -22,22 +22,22 @@ jfirst = ifirst;
 for (; jfirst < 58; jfirst++)
 {
 for (; j < 58; j++)
-	{
-	putchar(ifirst);
-	putchar(i);
-	putchar(' ');
-	putchar(jfirst);
-	putchar(j);
-	if (ifirst != 57 || jfirst != 57 || i != 56 || j != 57)
 {
-	putchar(',');
-	putchar(' ');
+putchar(ifirst);
+putchar(i);
+putchar(' ');
+putchar(jfirst);
+putchar(j);
+if (ifirst != 57 || jfirst != 57 || i != 56 || j != 57)
+{
+putchar(',');
+putchar(' ');
 }
 }
-	j = 48;
-		}
-		}
-	}
+j = 48;
+}
+}
+}
 putchar('\n');
 return (0);
 }
