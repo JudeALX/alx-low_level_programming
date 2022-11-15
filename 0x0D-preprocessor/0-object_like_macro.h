9996cf674d1025254bc6d59135f422cbea57e9be
JudeALX <okonkwoemekajude>
2022-11-15 01:01:02 -0800

    0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,10 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _OBJECT_LIKE_MACRO_
+#define _OBJECT_LIKE_MACRO_
 
-#include <stdio.h>
-#include "0-object_like_macro.h"
-#include "1-pi.h"
-#include "3-function_like_macro.h"
-#include "4-sum.h"
+#define SIZE 1024
 
 #endif
